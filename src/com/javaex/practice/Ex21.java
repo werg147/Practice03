fc314c4708955cd4ec5f9d3d0de92edacd90fcc3
werg1 <werg1@DESKTOP-9B10C7H>
2020-11-21 22:29:22 +0900

    깃허브 테스트

--- src/com/javaex/practice/Ex21.java
+++ src/com/javaex/practice/Ex21.java
@@ -5,6 +5,24 @@ public class Ex21 {
 	public static void main(String[] args) {
 		
 		System.out.println("깃허브 테스트");
+		
+
+		System.out.println("while문");
+		int index = 0;
+		while(index<3) {
+			System.out.println("index=" + index);
+			index++;
+		}
+		
+		System.out.println("for문");
+		for(int order=0; order<3; order++) {
+			System.out.println("order=" + order);
+		}
+		
+		
+		
+		
+		
 	}
 
 }
