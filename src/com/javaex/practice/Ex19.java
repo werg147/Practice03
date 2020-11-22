39213d48e55b59e8432d94504da4b8e3ed1526ab
werg1 <werg1@DESKTOP-9B10C7H>
2020-11-22 17:58:56 +0900

    반복문 연습문제

--- src/com/javaex/practice/Ex19.java
+++ src/com/javaex/practice/Ex19.java
@@ -9,31 +9,38 @@ public class Ex19 {
 		Scanner sc = new Scanner(System.in);
 		
 		int num;
-		int won;
-		int n1 = 0;
-		int n2;
-		int n3;
-		int n4;
-		
-		
-		
+		int won =0; 
 		
 		while(true) {
+			
 			System.out.println("----------------------------------");
 			System.out.println("1.예금 | 2.출금 | 3.잔고 | 4.종료");
 			System.out.println("----------------------------------");
 			System.out.print("선택>");
 			num = sc.nextInt();
-			System.out.print("예금액>");
-			won = sc.nextInt();
-				if(num==1) {
-					n1 = won+n1;
-					}
+			
+			if(num==1) {
+				System.out.print("예금액>");
+				won = sc.nextInt();
+				won = won + won;
+			} else if(num==2) {
+				System.out.print("출금액>");
+				won = sc.nextInt();
+				won = won - won;
+			} else if(num==3) {
+				System.out.println("잔고액>" + won);
+			} else if(num==4) {
+				System.out.println("프로그램 종료");
+				break;
+			} else {
+				System.out.println("다시 입력해주세요.");
+			}
+
 		}
 		
 		
+	
 		sc.close();
-
 	}
 
 }
