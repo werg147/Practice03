d43dfd4925ade938cfb586707f0a2618c741354a
werg1 <werg1@DESKTOP-9B10C7H>
2020-11-22 18:46:19 +0900

    반복문 연습문제

--- src/com/javaex/practice/Ex20.java
+++ src/com/javaex/practice/Ex20.java
@@ -1,10 +1,48 @@
 package com.javaex.practice;
 
+import java.util.Scanner;
+
 public class Ex20 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc =  new Scanner(System.in);
+		
+		int num = (int)(Math.random()*100)+1;
+		//int num = 89;
+		int no;
+		
+		System.out.println("========================");
+		System.out.println("[숫자맞추기게임 시작]");
+		System.out.println("========================");
+		
+		
+		while(true) {	
+			System.out.print(">>");
+			no = sc.nextInt();
+			
+			if(no<num) {
+				System.out.println("더 높게");
+			} else if(no>num) {
+				System.out.println("더 낮게");
+			} else {
+				System.out.println("맞았습니다.");
+				System.out.print("게임을 종료하시겠습니까?(y/n) >>");
+				String y = sc.nextLine();
+				if(y.equals("y") {
+					System.out.println("========================");
+					System.out.println("[숫자맞추기게임 종료]");
+					System.out.println("========================");
+					break;
+				}
+			}
+		
+		}
+		
+	
+		sc.close();
 
 	}
 
 }
+
